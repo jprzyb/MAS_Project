3da087f449dafe2e7f39d5c79bf2d7a3317a183b
Jakub Przybylski <jakubp02.10@gmail.com>
2024-06-04 21:02:29 +0200

    Views added. Documentation changed.

---
+++ src/main/java/pl/pjatk/mas/s24512/masproject/AnnualBonusController.java
@@ -0,0 +1,13 @@
+package pl.pjatk.mas.s24512.masproject;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AnnualBonusController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
